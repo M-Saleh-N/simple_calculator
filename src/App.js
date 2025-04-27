0f9bc0b1dea5abfd00287b97379b08b8ad3edb6c
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-27 10:57:29 +0300

    added new contents and buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Calculator from "./components/simple";
+
 
 function App() {
   return (
     <div className="App">
-
+      <Calculator/>
     </div>
   );
 }
