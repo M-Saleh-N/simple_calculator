04a60b39a821dc8ec361ec728ae9613c86ba35fd
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-27 10:56:24 +0300

    added conditions

--- src/components/simple.js
+++ src/components/simple.js
@@ -3,6 +3,20 @@ import "./simple.css";
 
 function Calculator() {
   const [input, setInput] = useState("");
+
+  const handleClick = (value) => {
+    if (value === "Clear") return setInput("");
+    if (value === "=") {
+      try {
+        setInput(eval(input).toString());
+      } catch {
+        setInput("Error");
+      }
+    } else {
+      setInput((prev) => prev + value);
+    }
+  };
+
 }
 
 export default Calculator;
