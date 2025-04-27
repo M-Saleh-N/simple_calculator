1ea1fd98f04e7dccca17ab1d145901b81cd99a62
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-27 10:56:42 +0300

    added numbers

--- src/components/simple.js
+++ src/components/simple.js
@@ -17,6 +17,13 @@ function Calculator() {
     }
   };
 
+  const buttons = [
+    "1", "2", "3", "-", 
+    "4", "5", "6", "+", 
+    "7", "8", "9", "*", 
+    "0", ".", "=", "/"
+  ];
+
 }
 
 export default Calculator;
